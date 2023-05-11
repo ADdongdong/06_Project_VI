b098d105362d4187efca946f77ed2ee41b7be165
headong <1035597870@qq.com>
2023-05-11 17:10:58 +0800

    modified:   03_experiment_demo/00_apple_dema/UHA.py 	modified:   03_experiment_demo/00_apple_dema/__pycache__/UHA.cpython-39.pyc 	modified:   03_experiment_demo/00_apple_dema/__pycache__/hvae.cpython-39.pyc 	modified:   03_experiment_demo/00_apple_dema/test.ipynb

--- 03_experiment_demo/00_apple_dema/UHA.py
+++ 03_experiment_demo/00_apple_dema/UHA.py
@@ -2,14 +2,22 @@ import torch
 import hvae 
 
 class UHA:
-    def __init__(self, dim, L_m=10, eps_m=0.1):
+    '''
+        参数：
+          f 能量函数，也就是用UHA来优化和采样的函数
+          dim 采样空间的维度
+          L_m 每次采样时，内部运行的leapforg算法的步数
+          eps_m 每次Leapfrog步长的大小
+    '''
+    def __init__(self, f, dim, L_m=10, eps_m=0.1):
         self.dim = dim
         self.L_m = L_m
         self.eps_m = eps_m
+        self.E = f 
 
     #定义能量函数E
-    def E(self, x):
-        return torch.sum(x ** 2) / 2
+    #def E(self, x):
+    #    return torch.sum(x ** 2) / 2
 
     #定义能量函数的梯度
     def grad_E(self, x):
@@ -33,7 +41,7 @@ class UHA:
         for i in range(self.L_m):
             #使用HMC步骤进行L_m次采样，并更新x和p
             x, p = self.HMC_step(x, p, self.eps_m)
-        return x
+        return x, p
 
     #采样函数
     def sample(self, num_samples=1):
@@ -47,15 +55,9 @@ class UHA:
                 # 从未校正的状态转移矩阵中采样(从转移核中采样)
                 y = self.UHA_step(x)
 
-                # 计算几首概率（在UHA中没有使用)
-                alpha = min(1., torch.exp(self.E(x) - self.E(y)))
-
-                # 更新当前状态（在UHA中没有使用)
-                if torch.rand(1) < alpha:
-                    x.data.copy_(y)
-
             # 保存在理目标分布的最终样本(在UHA中没有使用)
-            samples.append(x.detach().numpy())
+            samples.append(y.detach().numpy())
+
         return samples
 
     #定义函数从q(z|x)中采样
@@ -74,7 +76,12 @@ class UHA:
         '''
             函数返回值为采样的列表
         '''
+        pass
 
-uha = UHA(1)
+
+def E(x):
+    return torch.sum(x * 2) / 2
+
+uha = UHA(f=E, dim =1)
 sample = uha.sample(10)
 print(sample)
