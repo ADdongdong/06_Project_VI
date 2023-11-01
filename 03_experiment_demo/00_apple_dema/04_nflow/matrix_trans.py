c7f626e323df85dff19555dce2ec3dd9bd7fae76
fatTiger <50684396+ADdongdong@users.noreply.github.com>
2023-11-01 16:18:50 +0800

    commit

--- 03_experiment_demo/00_apple_dema/04_nflow/matrix_trans.py
+++ 03_experiment_demo/00_apple_dema/04_nflow/matrix_trans.py
@@ -8,26 +8,29 @@ from openpyxl import Workbook
 
 # 将数据的处理定义成一个类
 # 这个类接收到的数据是一个numpy.array
-# 这个array中，有多个8*8的二维array
-# 所以，初始化对象的时候，可以传入一个(n, 8, 8)shape的数据
-class matrix_data():
-    def __init__(self, data) -> None:
-        self.data = data
+# 这个array中，有多个self.size*self.size的二维array
+# 所以，初始化对象的时候，可以传入一个(n, self.size, self.size)shape的数据
 
-    # private:输入一个8*8的array,对这个8*8的array进行处理
+
+class matrix_data():
+    def __init__(self, data, size) -> None:
+        self.data = data  # 传入打分矩阵列表
+        self.size = size  # 传入每个矩阵的大小
+
+    # private:输入一个self.size*self.size的array,对这个self.size*self.size的array进行处理
     def __data_trans(self, data):
         # 对矩阵的每一个数字进行如下计算：
         # i = power(sqre(2), i-2)
-        for j in range(8):
-            for k in range(8):
+        for j in range(self.size):
+            for k in range(self.size):
                 data[j][k] = np.power(np.sqrt(2), data[j][k] - 2)
         # 将矩阵的中间元素转化1
         np.fill_diagonal(data, 1.0)
         # print(data)
 
         # 补齐矩阵的上三角
-        for i in range(8):
-            for j in range(i + 1, 8):
+        for i in range(self.size):
+            for j in range(i + 1, self.size):
                 data[i][j] = 1/data[j][i]
         return data
 
@@ -39,7 +42,7 @@ class matrix_data():
         # 因为list中有元素为0，所以，这里给所有的元素+1
         for i in self.data:
             i = i.astype(float)
-            i += 1.0  # 个矩阵的所有元素+1 打分的范围从1-8
+            i += 1.0  # 个矩阵的所有元素+1 打分的范围从1-self.size
             data = self.__data_trans(i)
             result.append(data)
             # break
@@ -47,7 +50,7 @@ class matrix_data():
         np.save(filename, result)
         return result
 
-    # 将多个8*8的array保存在xlsx表格中
+    # 将多个self.size*self.size的array保存在xlsx表格中
     def save_excel(self, data,  file_name: str) -> None:
         # 创建一个工作簿
         wb = Workbook()
@@ -57,7 +60,7 @@ class matrix_data():
         row_num = 1
         # 逐个写入每个张量到工作表中，并在每个张量之后插入一个空行
         for tensor in data:
-            # 将 8x8 的张量转换为 DataFrame
+            # 将 self.size 的张量转换为 DataFrame
             df = pd.DataFrame(tensor)
 
             # 写入 DataFrame 数据到工作表中
@@ -73,16 +76,16 @@ class matrix_data():
 
     # 将经过上面处理的数据再经过归一化，处理为可以放入正则化流中学习的数据
     def to_normalizing_flow_data(self, data) -> list:
-        # 对于每一个矩阵，计算其集几何平均值：将8个元素连续乘，然后对结果开8次方
+        # 对于每一个矩阵，计算其集几何平均值：将self.size个元素连续乘，然后对结果开self.size次方
         result = []
         for matrix in data:
-            x = []  # 这个数组用来记录每一个矩阵的8个几何平均值
-            for i in matrix:  # 一个matrix里面有8行，就会计算出来8个元素
+            x = []  # 这个数组用来记录每一个矩阵的self.size个几何平均值
+            for i in matrix:  # 一个matrix里面有self.size行，就会计算出来self.size个元素
                 num = 1
-                for j in range(8):
+                for j in range(self.size):
                     num = i[j]*num
                 # 对num开n次方
-                num = num**(1/8)
+                num = num**(1/self.size)
                 x.append(num)
             result.append(x)
 
@@ -97,6 +100,6 @@ class matrix_data():
         normalized_result = np.array(normalized_result)
         final_list = []
         # 将这个矩阵，按照列保存
-        for i in range(8):
+        for i in range(self.size):
             final_list.append(normalized_result[:, i])
         return final_list
